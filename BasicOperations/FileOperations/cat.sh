aa082f8907d5a1defc92f3d7b3f96631dcdaa39b
KiLJ4EdeN <parsa592323@gmail.com>
2020-10-12 14:44:53 +0330

    .

---
+++ BasicOperations/FileOperations/cat.sh
@@ -0,0 +1,10 @@
+# see contents of a text file
+cat test1.txt
+# see contents of two files
+cat test1.txt test2.txt
+# concatenate two text files into a new file.
+cat test1.txt test2.txt > test3.txt
+# copy contents of test1.txt into test2.txt
+cat < test1.txt > test2.txt
+
+
---
+++ BasicOperations/FileOperations/chmod.sh
@@ -0,0 +1,6 @@
+# change mode: allows reading, writing and execution of your files.
+# example: to run this file (.sh file) we need to run:
+# chmod +x chmod.sh
+# this gives the user the permission to eXecute the bash file.
+echo "Script was ran Successfully."
+
