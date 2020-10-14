4cfba1d471d6667521fa030f08436365d732d506
KiLJ4EdeN <parsa592323@gmail.com>
2020-10-14 15:21:16 +0330

    echo

--- BasicOperations/FileOperations/cat.sh
+++ BasicOperations/FileOperations/cat.sh
@@ -1,10 +1,15 @@
-# see contents of a text file
+echo "'cat' lets you see the contents of a text file"
+echo "Usages:"
+echo "cat filename"
 cat test1.txt
-# see contents of two files
+echo "see contents of two files"
+echi "cat file1 file2"
 cat test1.txt test2.txt
-# concatenate two text files into a new file.
+echo "concatenate two text files into a new file."
+echo "cat file1 file2 > file3"
 cat test1.txt test2.txt > test3.txt
-# copy contents of test1.txt into test2.txt
+echo "copy contents of test1.txt into test2.txt"
+echo "cat < file1 > file2"
 cat < test1.txt > test2.txt
 
 
--- BasicOperations/FileOperations/chmod.sh
+++ BasicOperations/FileOperations/chmod.sh
@@ -1,6 +1,6 @@
-# change mode: allows reading, writing and execution of your files.
-# example: to run this file (.sh file) we need to run:
-# chmod +x chmod.sh
-# this gives the user the permission to eXecute the bash file.
+echo "change mode: allows reading, writing and execution of your files."
+echo "example: to run this file (.sh file) we need to run:"
+echo "chmod +x chmod.sh"
+echo "this gives the user the permission to eXecute the bash file."
 echo "Script was ran Successfully."
 
