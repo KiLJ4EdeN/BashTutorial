b865a3547a876692d6c5c480e2d50177cebf8e02
KiLJ4EdeN <parsa592323@gmail.com>
2020-10-14 15:11:56 +0330

    cp and chown

---
+++ BasicOperations/FileOperations/chown.sh
@@ -0,0 +1,3 @@
+echo "Change Owner of a file or folder."
+echo "Usage:"
+echo "chown -options user:group filename"
---
+++ BasicOperations/FileOperations/cp.sh
@@ -0,0 +1,6 @@
+echo "'cp' copies files or folders from one location to another"
+echo "Usage:"
+echo "cp file1 file2"
+echo "cp file1 folder1/"
+
+cp test1.txt texts/
